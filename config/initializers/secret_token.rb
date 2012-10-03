07c4c4b447fc81446b1998934d1de059ba6b792d
Dmytro Komisaryk <dmytrok@interlink-ua.com>
2012-10-03 14:38:01 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = 'd7c9fedadb53d57333a16aa6656cddf877fae4e707d508d2fc1487152c65cb057482e5973c4bce447174a9efd78ac4e13eeb8f0151dff4814f8c2ca2e05e6a0f'
