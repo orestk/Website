055bb4561a157e72e55a3f5ab748ccd140e13670
Dmytro Komisaryk <dmytrok@interlink-ua.com>
2012-10-03 17:14:13 +0300

    create view(html with table)

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,7 @@ Website::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'cats#index'
+  resources :cats
+   match ':controller(/:action(/:id))(.:format)'
 end
