90e10dc59e57dad9efba041426087b371dcaa554
Dmytro Komisaryk <dmytrok@interlink-ua.com>
2012-10-03 17:33:30 +0300

    new page with form for adding new cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,4 +2,14 @@ class CatsController < ApplicationController
   def index
     @cats=Cat.all
   end
+
+  def new
+  @cat=Cat.new
+  end
+
+  def create
+    Cat.create(params[:cat])
+    redirect_to root_path
+  end
+
 end
